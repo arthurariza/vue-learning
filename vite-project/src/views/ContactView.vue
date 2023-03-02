60f08381a5a9a8676eec5a54e18e36a44f6fb100
arthurariza <tucaariza@gmail.com>
2023-03-02 19:58:17 -0300

    DefineProps and DefineEmits

--- vite-project/src/views/ContactView.vue
+++ vite-project/src/views/ContactView.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="contact">
     <h1>This is an contact page</h1>
+    <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;"/>
   </div>
 </template>
+
+<script setup>
+import {ref} from "vue";
+import TabbableTextarea from "@/components/TabbableTextarea.vue";
+
+let comment = ref('initial textarea value');
+</script>
 
 <style>
 @media (min-width: 1024px) {
