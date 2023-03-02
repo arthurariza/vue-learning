aef50331a781e1878f10f84a5fb4e248ef0f2d9d
arthurariza <tucaariza@gmail.com>
2023-03-01 21:04:25 -0300

    Composable Example: Local Storage

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 import {useFlash} from '@/composables/useFlash.js'
+import {useStorage} from "@/composables/useStorage";
 
 let {flash} = useFlash();
+let food = useStorage('food');
 </script>
 
 <template>
@@ -12,6 +14,9 @@ let {flash} = useFlash();
       <button @click="flash('Title', 'Test Message')">
         Click Me!
       </button>
+    </p>
+    <p>
+      What is your favorite food? <input v-model="food" type="text">
     </p>
   </main>
 </template>
