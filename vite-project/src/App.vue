b910b931e21906e2254db279f61515837eb9cfb6
arthurariza <tucaariza@gmail.com>
2023-03-06 15:09:15 -0300

    Direct Mutation Concerns

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'</script>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/contact">Contact</RouterLink>
         <RouterLink to="/quiz">Quiz</RouterLink>
+        <RouterLink to="/counter">Counter</RouterLink>
       </nav>
     </div>
   </header>
---
+++ vite-project/src/views/CounterView.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>Counter: {{ counter.count }}</h1>
+    <button @click="counter.increment()">Increment</button>
+  </section>
+</template>
+
+<script setup>
+import {counter} from "@/stores/counterStore";</script>
+
+<style scoped>
+
+</style>
