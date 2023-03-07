03fb82731a3ebdf360e065e9ccfd8b86990210b5
arthurariza <tucaariza@gmail.com>
2023-03-07 09:40:16 -0300

    Add TeamDashboard

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-import {RouterLink, RouterView} from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'</script>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";</script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" height="125" src="@/assets/logo.svg" width="125"/>
+    <img alt="Vue logo" class="logo" height="125" src="@/assets/logo.svg" width="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!"/>
+      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -15,17 +15,19 @@ import HelloWorld from './components/HelloWorld.vue'</script>
         <RouterLink to="/contact">Contact</RouterLink>
         <RouterLink to="/quiz">Quiz</RouterLink>
         <RouterLink to="/counter">Counter</RouterLink>
+        <RouterLink to="/team">Team</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView/>
+  <RouterView />
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
+  margin-bottom: 2rem;
 }
 
 .logo {
@@ -58,30 +60,5 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ vite-project/src/views/TeamView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import team from "@/team.json";
+import TeamHeader from "@/components/TeamHeader.vue";
+import TeamMembers from "@/components/TeamMembers.vue";
+import TeamFooter from "@/components/TeamFooter.vue";
+</script>
+
+<template>
+  <TeamHeader :team="team" />
+
+  <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
+    <TeamMembers :team="team" />
+  </div>
+
+  <TeamFooter :team="team" />
+</template>
